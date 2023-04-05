dad629b8e5e081df14f9b693690f73d13696ec7e
unknown <Administrator@LSST-AD-IT12.londonsst.ac>
2023-04-05 15:43:14 +0100

    first commit

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,58 @@
+<template>
+  <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full">
+    <div class="container">
+      <div class="container mx-auto">
+        <nav class="p-4 flex items-center justify-between ">
+          <div class="text-lg font-medium ">
+            <Link :href="route('listing.index')">Listings</Link>
+          </div>
+          <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
+            <Link :href="route('listing.index')">LaraZillow</Link>
+          </div>
+          <div v-if="user" class="flex items-center gap-4">
+            <Link :href="route('realtor.listing.index')" class="text-sm text-gray-500">{{ user.name }}</Link>
+
+            <Link :href="route('realtor.listing.create')" class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">+ New Listing</Link>
+            <div>
+              <Link :href="route('logout')" method="delete" as="button">Logout</Link>
+            </div>
+          </div>
+          <div v-else class="flex items-center gap-2">
+            <Link :href="route('user-account.create')">Register</Link>
+            <Link :href="route('login')">Sign-in</Link>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </header>
+
+  <main class="container mx-auto p-4 w-full">
+    <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
+      {{ flashSuccess }}
+    </div>
+
+
+
+
+    <slot>Default</slot>
+  </main>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import {Link, usePage} from '@inertiajs/vue3'
+
+
+
+
+const flashSuccess = computed(
+  () => usePage().props.flash.success,
+)
+
+const user = computed(
+  () => usePage().props.user,
+)
+
+
+
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,16 @@
+<template>
+
+
+        <div>Show</div>
+        <Link href="/"> Main Page </Link>
+
+
+
+</template>
+
+<script setup>
+import {Link} from '@inertiajs/vue3'
+
+</script>
+
+
